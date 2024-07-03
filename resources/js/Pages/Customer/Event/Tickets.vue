0573d4c0322aa05c28bf37b8a99457f174168ea4
Joren Sumagang <sumagangjoren@gmail.com>
2024-07-03 11:13:36 +0800

    qr code can be downloaded

--- resources/js/Pages/Customer/Event/Tickets.vue
+++ resources/js/Pages/Customer/Event/Tickets.vue
@@ -27,6 +27,11 @@ defineProps({
                 <q-card-section v-for="ticket_order_item in ticket_order.ticket_order_items">
                     {{ ticket_order_item }}
                 </q-card-section>
+                <a :href="`/storage/${ticket_order.qr_code_path}`" download>
+                    <q-btn>
+                        Download Qr Code
+                    </q-btn>
+                </a>
             </q-card>
         </div>
     </div>
